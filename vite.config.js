0468ad1d83cd22c1370b9ebd04b7c90a077e1d2b
Leone Bastos <leone.dev5@gmail.com>
2026-02-20 10:19:23 -0300

    fix(roteamento e basepath): corrige redirecionamento de roteamento xamp

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "forms_atom",
+  base: "/forms_atom",
   plugins: [vue()],
   build: {
     outDir: "forms_atom"
