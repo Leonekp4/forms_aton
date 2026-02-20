e98a177a00899fe6efb6f2fa6e3c13bca765ba24
Leone Bastos <leone.dev5@gmail.com>
2026-02-20 10:07:56 -0300

    fix(vite): atualiza build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "forms_atom",
   plugins: [vue()],
+  build: {
+    outDir: "forms_atom"
+  }
 })
